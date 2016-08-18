766752cedb3e052f9e2653954afa839cd38d4279
Marvin Bertin <marvin.bertin@gmail.com>
2016-08-17 19:23:34 -0700

    added data_loader & Label_term_plotter

--- 1-Label_Term_Summary_Statistics/utils/data_loader.py
+++ 1-Label_Term_Summary_Statistics/utils/data_loader.py
@@ -7,8 +7,8 @@ class DataLoader(object):
     """docstring for data_loader"""
     def __init__(self):
         pass
-        
-    def load_data(fname):
+
+    def load_data(self, fname):
 
         with open(fname) as f:
             content = f.readlines()
@@ -16,12 +16,12 @@ class DataLoader(object):
         print("Number of Sentences: ", content[0])
         return content
 
-    def _extract_data(line):
+    def _extract_data(self, line):
         return (line[0], line[1:])
 
-    def get_data_frame(content, label_names):
+    def get_data_frame(self, content, label_names):
 
-        data = list(map(_extract_data, content[1:]))
+        data = list(map(self._extract_data, content[1:]))
         label_idx = np.array(list(map(lambda line: int(line[0])-1, data)))
         labels = label_names[label_idx]
         sentences = list(map(lambda line: line[1].strip(), data))
