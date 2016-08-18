a3b86ddfa18e7432d2c55bf795a1833780f5d178
Marvin Bertin <marvin.bertin@gmail.com>
2016-08-18 13:53:12 -0700

    add term_fre_helper and data_frame_helper

---
+++ 1-Label_Term_Summary_Statistics/utils/data_frame_helper.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class DataFrameHelper(object):
+	"""docstring for PlotHelper"""
+	def __init__(self, dataframe, label_names):
+		self.df = dataframe
+		self.label_names = label_names
+		self.groupby_label_df = self.groupby_label()
+
+
+	def _data_transform(self, data):
+	    return pd.Series({"Label_freq": data["Labels"].count(),
+	                      "Text_concat": " ".join(data["Text"])
+	                     })
+
+	def groupby_label(self):
+		self.document_df = self.df.groupby("Labels").apply(self._data_transform)
+		self.corpus_matrix = self.document_df["Text_concat"].as_matrix()
+		return self.document_df
+
+	def plot_label_distribution(self):
+		plt.figure(figsize=(10, 8))
+		sns.countplot(x="Labels", data=self.df, palette="Paired")
+		plt.title("Class Label Distribution", fontsize=24)
+		plt.show()
+
+	def global_corpus_summary(self, doc_term_matrix, min_df):
+		label_num, vocab_size = doc_term_matrix.shape
+		print("Data contains {} topic labels with {} unique words (minimum frequency of {})"
+		      .format(label_num, vocab_size, min_df))
+
+	def plot_terms_per_label(self, doc_term_matrix, unique = True):
+	    label_num, _ = doc_term_matrix.shape
+	    
+	    if unique:
+	        name = "Unique"
+	        terms = np.array([doc_term_matrix.getrow(row).nnz for row in range(label_num)])
+	    else:
+	        name = "Total"
+	        terms = doc_term_matrix.sum(axis = 1).getA().squeeze()
+	    
+	    df_terms = pd.DataFrame({"Labels": self.label_names,
+	                             "{}_Terms".format(name): terms})
+	    
+	    plt.figure(figsize=(10, 8))
+	    sns.barplot(x="Labels", y="{}_Terms".format(name), data=df_terms, palette="Paired")
+	    plt.title("Number of {} Terms Per Label".format(name), fontsize=24)
+	    plt.ylabel("Count")
+	    plt.show()
+		
---
+++ 1-Label_Term_Summary_Statistics/utils/term_frequency_helper.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer
+
+class TermFrequency(object):
+	"""docstring for TermFrequency"""
+	def __init__(self,
+		label_names,
+		lowercase = True,
+		preprocessor = None,
+		tokenizer = None,
+		stop_words = None,
+		ngram_range = (1, 1),
+		analyzer = 'word',
+		max_df = 1.0,
+		min_df = 1,
+		max_features = None,
+		vocabulary = None):
+
+		self.vectorizer = CountVectorizer(
+			lowercase = lowercase,
+			preprocessor = preprocessor,
+			tokenizer = tokenizer,
+			stop_words = stop_words,
+			ngram_range = ngram_range,
+			analyzer = analyzer,
+			max_df = max_df,
+			min_df = min_df,
+			max_features = max_features,
+			vocabulary = vocabulary)
+
+		self.min_df = min_df
+		self.label_names = label_names
+		
+	def vectorize_corpus(self, corpus):
+		self.X = self.vectorizer.fit_transform(corpus)
+		self.vocab = np.array(self.vectorizer.get_feature_names())
+
+	def _bottom_n_filter(self, word_sorted, count2idx, n):
+	    
+	    bottom_n = []
+	    bottom_n_frq = []
+	    counter = 0
+	    
+	    for idx in word_sorted:
+	        if count2idx[idx] >= self.min_df:
+	            bottom_n.append(idx)
+	            bottom_n_frq.append(count2idx[idx])
+	            counter += 1
+	        if counter == n:
+	            break
+	    return (bottom_n, bottom_n_frq)
+
+	def _top_bottom_n_terms(self, word_count, n):
+
+	    count2idx = dict(enumerate(word_count))
+	    word_sorted = word_count.argsort()
+
+	    top_n = word_sorted[-n:]
+	    top_n_frq = [count2idx[idx] for idx in top_n]
+
+	    bottom_n, bottom_n_frq = self._bottom_n_filter(word_sorted, count2idx, n)
+
+	    return (list(zip(top_n, top_n_frq)), list(zip(bottom_n,bottom_n_frq)))
+
+	def _term_freq_data_frame(self, term_freq):
+	    terms = np.array([x[0] for x in term_freq])
+	    freq = np.array([x[1] for x in term_freq])
+	    
+	    return pd.DataFrame({"Terms": self.vocab[terms],
+	                         "Count": freq})
+
+	def _plot_term_freq(self, data_frame, ax, palette):
+	    ax = sns.barplot(y="Terms", x="Count", palette=palette, data=data_frame, ax=ax)
+	    ax.set(xlabel='Term Frequency', ylabel='')
+
+	def run_term_freq_plot(self, word_count, N, title, figsize=(15, 10)):
+
+	    top_n, bottom_n = self._top_bottom_n_terms(word_count, N)
+
+	    f, axes = plt.subplots(1, 2, figsize=figsize, sharex=False)
+	    f.suptitle(title, fontsize=25)
+
+	    data_bottom = self._term_freq_data_frame(bottom_n)
+	    self._plot_term_freq(data_bottom, axes[0], "Blues")
+
+	    data_top = self._term_freq_data_frame(top_n)
+	    self._plot_term_freq(data_top, axes[1], "Reds")
+
+	def plot_term_freq_dist(self, N, per_label = False):
+		
+		if per_label:
+			for idx, label in enumerate(self.label_names):
+				word_count = self.X.getrow(idx).toarray().squeeze()
+				self.run_term_freq_plot(
+					word_count,
+					N,
+					title = "Top N & Bottom N Term Frequency (Label {})".format(label),
+					figsize=(15, 5))
+		else:
+			word_count = self.X.sum(axis = 0).getA().squeeze()
+			self.run_term_freq_plot(
+				word_count,
+				N,
+				title = "Top N & Bottom N Term Frequency (Overall)",
+				figsize=(15, 10))
+
+
