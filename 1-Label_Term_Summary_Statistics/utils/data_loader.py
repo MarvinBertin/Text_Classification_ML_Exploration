e4e8c190f38e6d2ea228c905901d08fd70deb1d4
Marvin Bertin <marvin.bertin@gmail.com>
2016-08-17 18:13:59 -0700

    add folder

---
+++ 1-Label_Term_Summary_Statistics/utils/data_loader.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class DataLoader(object):
+    """docstring for data_loader"""
+    def __init__(self):
+        pass
+        
+    def load_data(fname):
+
+        with open(fname) as f:
+            content = f.readlines()
+
+        print("Number of Sentences: ", content[0])
+        return content
+
+    def _extract_data(line):
+        return (line[0], line[1:])
+
+    def get_data_frame(content, label_names):
+
+        data = list(map(_extract_data, content[1:]))
+        label_idx = np.array(list(map(lambda line: int(line[0])-1, data)))
+        labels = label_names[label_idx]
+        sentences = list(map(lambda line: line[1].strip(), data))
+
+        return pd.DataFrame({"Labels": labels, "Text": sentences})
